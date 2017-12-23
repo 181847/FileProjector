3e512096f04f16f2d1016b201239aa3fdce2a29f
181847 <753090084@qq.com>
2017-12-23 15:57:01 +0800

    Revert "无聊的修改，无法修复的Bug，窗口无法响应拖拽事件，准备回退版本。"
    This reverts commit 119d7d4c51458a180afe81d676ff5125217a1c4a.

--- FileProjector/MainPage.xaml.cs
+++ FileProjector/MainPage.xaml.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
-using Windows.ApplicationModel;
 using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -26,23 +25,83 @@ namespace FileProjector
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        // 当前正在处理的源文件夹
+        private IStorageFolder currSourceFolder = null;
+        // 当前正在处理的目标文件夹
+        private IStorageFolder currDestFolder = null;
+        
         public MainPage()
         {
             this.InitializeComponent();
         }
 
-        private void sourceFileButton_DragEnter(object sender, DragEventArgs e)
+        // 检查并且设置DragOver事件的数据，只接受包含文件夹的拖拽。
+        private async void CheckDragOverData_OnlyFolderAccept(DragEventArgs e)
         {
-            e.AcceptedOperation = DataPackageOperation.Copy;
-
-        }
-
-        private void sourceFileButton_Drop(object sender, DragEventArgs e)
-        {
+            e.AcceptedOperation = Windows.ApplicationModel.DataTransfer.DataPackageOperation.Copy;
+            bool haveFolder = false;
+            // 检查拖拽项目中是否存在文件夹
             if (e.DataView.Contains(StandardDataFormats.StorageItems))
             {
-                sourceFolderPathText.Text = "终于有文件了";
+                var items = await e.DataView.GetStorageItemsAsync();
+                foreach (var stgItem in items)
+                {
+                    if (stgItem.IsOfType(StorageItemTypes.Folder))
+                    {
+                        haveFolder = true;
+                    }
+                }
+                
             }
+
+            // 只有拖拽项目中包含文件夹才处理接下来的流程。
+            if (haveFolder)
+            {
+                e.AcceptedOperation = DataPackageOperation.Copy;
+                e.DragUIOverride.Caption = "放置文件夹";
+            }
+            // 否则不接受拖拽。
+            else
+            {
+                e.AcceptedOperation = DataPackageOperation.None;
+                e.DragUIOverride.Caption = "无法处理指定项目，请拖拽文件夹至此";
+            }
+        }
+
+        // 获取Drop事件数据中的第一个文件夹。
+        private async Task<IStorageFolder> GetFirstStorageFolderFromDragEvent(DragEventArgs e)
+        {
+            IStorageFolder firstFolder = null;
+            if (e.DataView.Contains(StandardDataFormats.StorageItems))
+            {
+                var items = await e.DataView.GetStorageItemsAsync();
+                foreach (var stgItem in items)
+                {
+                    // 如果找到了一个Folder，记录，并且终止循环。
+                    if (stgItem.IsOfType(StorageItemTypes.Folder))
+                    {
+                        firstFolder = stgItem as IStorageFolder;
+                        break;
+                    }
+                }
+            }
+#if DEBUG
+            if (currSourceFolder == null)
+            {
+                throw new Exception("异常，没有从Drop事件中发现文件夹数据。");
+            }
+#endif
+            return firstFolder;
+        }
+
+        private void sourceFileButton_DragOver(object sender, DragEventArgs e)
+        {
+            CheckDragOverData_OnlyFolderAccept(e);
+        }
+
+        private async void sourceFileButton_Drop(object sender, DragEventArgs e)
+        {
+            currSourceFolder = await GetFirstStorageFolderFromDragEvent(e);
         }
     }
 }
