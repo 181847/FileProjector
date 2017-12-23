119d7d4c51458a180afe81d676ff5125217a1c4a
181847 <753090084@qq.com>
2017-12-23 15:56:55 +0800

    无聊的修改，无法修复的Bug，窗口无法响应拖拽事件，准备回退版本。

--- FileProjector/BlankPage1.xaml.cs
+++ FileProjector/BlankPage1.xaml.cs
@@ -3,12 +3,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using System.Threading.Tasks;
-using Windows.ApplicationModel;
 using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -17,32 +14,33 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace FileProjector
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class BlankPage1 : Page
     {
-        public MainPage()
+        public BlankPage1()
         {
             this.InitializeComponent();
         }
+    private void sourceFileButton_DragEnter(object sender, DragEventArgs e)
+    {
+        e.AcceptedOperation = DataPackageOperation.Copy;
 
-        private void sourceFileButton_DragEnter(object sender, DragEventArgs e)
+    }
+
+    private void sourceFileButton_Drop(object sender, DragEventArgs e)
+    {
+        if (e.DataView.Contains(StandardDataFormats.StorageItems))
         {
-            e.AcceptedOperation = DataPackageOperation.Copy;
-
-        }
-
-        private void sourceFileButton_Drop(object sender, DragEventArgs e)
-        {
-            if (e.DataView.Contains(StandardDataFormats.StorageItems))
-            {
-                sourceFolderPathText.Text = "终于有文件了";
-            }
+            sourceFolderPathText.Text = "终于有文件了";
         }
     }
+    }
+
+
 }
